1858da5af5c423b2d652a96a14a2ed128bf5c522
Sophia Filimonova <sophfil708@gmail.com>
2023-06-08 22:42:40 +0300

    update settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -29,7 +29,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '*',
+    'webserver',
+    '.railway.app'
+]
 
 
 # Application definition
